10ac9247eea8869e4f81e832c0fc246e54a44bfe
hama1185 <hamataku0120@gmail.com>
2021-08-25 11:30:55 +0900

    画面を取得

---
+++ Assets/Scripts/webcam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class webcam : MonoBehaviour
+{
+    public RenderTexture Capture;
+
+    WebCamTexture webCamTexture;
+
+    void Start () {
+        webCamTexture = new WebCamTexture();
+        webCamTexture.Play();
+	}
+	
+	void Update (){
+        // ビットマップの転送
+        Graphics.Blit(webCamTexture, Capture);
+	}
+}
