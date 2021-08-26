4497a2703e9b2b4ab7d14833a807284b6bc96496
hama1185 <hamataku0120@gmail.com>
2021-08-26 10:34:32 +0900

    写真を取って保存するのを実装

---
+++ Assets/Scripts/takePicture.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class takePicture : MonoBehaviour
+{
+    public RenderTexture CamTex;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void onclickTakeButton(){
+
+        Texture2D tex = new Texture2D(CamTex.width, CamTex.height, TextureFormat.RGB24, false);
+        RenderTexture.active = CamTex;
+        tex.ReadPixels(new Rect(0, 0, CamTex.width, CamTex.height), 0, 0);
+        tex.Apply();
+
+        byte[] bytes = tex.EncodeToPNG();
+        File.WriteAllBytes("CameraView.png", bytes);
+    }
+}
